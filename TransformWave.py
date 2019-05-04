f4fcf6739bbd98668977aa06c4503027111a209c
yuliangkuocs <yuliangkuocs@gmail.com>
2019-05-04 11:57:31 +0800

    init commit

---
+++ TransformWave.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+from glob import glob
+from scipy.io.wavfile import write
+
+
+DATA_PATH = './WaveData/'
+SAMPLE_RATE = 16000
+
+waveDatas = glob(DATA_PATH + '*.npy')
+
+
+for data in waveDatas:
+    outputPath = os.path.dirname(__file__) + '/Result/'
+    waveNpData = np.load(data)
+    write(outputPath + 'result-{0}.wav'.format(data.replace(DATA_PATH, '').replace('.npy', '')), SAMPLE_RATE, waveNpData)
+
+
+'''LPCNET trying by Ben on Friday'''
+# import os
+# import numpy as np
+# from glob import glob
+# from scipy.io.wavfile import write
+#
+#
+# DATA_PATH = './WaveData/'
+# SAMPLE_RATE = 16000
+#
+# waveDatas = glob(DATA_PATH + '*.npy')
+#
+#
+# for data in waveDatas:
+#     outputPath = os.path.dirname(__file__) + '/Result/'
+#     waveNpData = np.load(data)
+#     waveNpData = waveNpData.reshape((-1,))
+#     waveNpData.tofile("mel_220k_0.s16")
+
+
