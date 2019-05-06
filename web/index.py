29a73b557c032727314312e235300618e0629f85
yuliangkuocs <yuliangkuocs@gmail.com>
2019-05-06 22:23:57 +0800

    init commit

---
+++ web/index.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return '<h1>This is Index Page</h1>'
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080)
+
