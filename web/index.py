a885b6b675d77f1a8b17f8121423e4610976e55e
yuliangkuocs <yuliangkuocs@gmail.com>
2019-05-06 23:11:30 +0800

    init commit

--- web/index.py
+++ web/index.py
@@ -9,6 +9,12 @@ def index():
 	return '<h1>This is Index Page</h1>'
 
 
+@app.route('/aboutus/')
+def aboutus():
+	return '<h1>About Us</h1>'
+
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
 
