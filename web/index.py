a0be13b46997aad97977f3272ca710fa364d545f
yuliangkuocs <yuliangkuocs@gmail.com>
2019-05-09 15:18:00 +0800

    test

--- web/index.py
+++ web/index.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, send_file, jsonify
+from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user
 
 
 app = Flask(__name__)
+app.secret_key = '7433a508b0a1ade2faea975e'
+
+login_manager - LoginManager(app)
+users = {'admin': {'password': 'admin'}}
+
+
+class User(UserMixin):
+	pass
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+	if user_id not in users:
+		return
+	user = User()
+	user.id = user_id
+	return user
+
 
 # Page
 @app.route('/')
@@ -21,8 +40,22 @@ def private_policy():
 
 @app.route('/login/')
 def login():
-	return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	
+	user_id = request.form['id']
+	if request.form['password'] == users[user_id]['password']:
+		user = User()
+		user.id = user_id
+		login_user(user)
+		return redirect(url_for('protect'))
+	return 'Login Fail'
 
+
+@app.route('/protect/')
+def protect():
+	if current_user.is_active:
+		return 'Logged in as: ' + current_user.id
 
 @app.route('/logout/')
 def logout():
@@ -42,22 +75,32 @@ def steven():
 # API
 @app.route('/api/demo/getAudio', methods=['POST'])
 def api_demo_getAudio():
-	filePath = '../data/tacotron-ch-92000-0.wav'
-
+	filePath = 'static/test.wav'
+	
+	# return jsonify({'response': 'hi'}), 200
 	return jsonify(send_file(filePath, 'audio/wav', True, 'tacotron-ch-92000-0.wav')), 200
 
 # API using Response
-@app.route("/api/demo/wav")
-def api_demo_wav():
-    def generate():
-        with open("../static/test.wav") as fwav:
-            data = fwav.read(1024)
-            while data:
-                yield data
-                data = fwav.read(1024)
-    return Response(generate(), mimetype="audio/x-wav")
+#@app.route('/api/demo/wav', methods=['GET', 'POST'])
+#def streamwav():
+#	def generate():
+#        	with open("/static/test.wav", "rb") as fwav:
+#	           	data = fwav.read(1024)
+#        	    	while data:
+#                		yield data
+#                		data = fwav.read(1024)
+#    return Response(generate(), mimetype="audio/x-wav")
 
+# API Steven test 2
+@app.route('/api/test2', methods=['POST'])
+def view_method():
+     path_to_file = "/static/test.wav"
 
+     return send_file(
+         path_to_file, 
+         mimetype="audio/wav", 
+         as_attachment=True, 
+         attachment_filename="test.wav")
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
