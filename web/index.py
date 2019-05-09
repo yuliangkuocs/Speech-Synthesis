0b411c1af582c2d3a02f7cb4e30e483373bd8ae7
Hank Kuo <hank@Hankde-MacBook-Pro.local>
2019-05-09 16:55:43 +0800

    test login

--- web/index.py
+++ web/index.py
@@ -3,96 +3,103 @@ from flask import Flask, render_template, send_file, jsonify, request, redirect,
 app = Flask(__name__)
 app.secret_key = '7433a508b0a1ade2faea975e'
 
-
 users = {'admin': {'password': 'admin'}}
 
 
 # Page
 @app.route('/')
 def home():
-	if 'user_id' in session:
-		return redirect(url_for('text_to_speech'))
-	return render_template('home.html')
+    if 'user_id' in session:
+        return redirect(url_for('text_to_speech'))
+    return render_template('home.html')
 
 
 @app.route('/about-us/')
 def about_us():
-	return render_template('about-us.html')
+    return render_template('about-us.html')
 
 
 @app.route('/text-to-speech/')
 def text_to_speech():
-	if 'user_id' in session:
-		return render_template('text-to-speech.html')
-	else:
-		return '<h1>Bad Login</h1>'
+    if 'user_id' in session:
+        return render_template('text-to-speech.html')
+    else:
+        return render_template('not-login.html')
 
 
 @app.route('/private-policy/')
 def private_policy():
-	return render_template('private-policy.html')
+    return render_template('private-policy.html')
 
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login/')
 def login():
-	if request.method == 'GET':
-		return render_template('login.html')
-
-	request_data = request.get_json()
-	print(request_data)
-	if request_data['id'] in users and request_data['password'] == users[request_data['id']]['password']:
-		session['user_id'] = request_data['id']
-		return jsonify({'response': 'Login Success'}), 200
-
-	return jsonify({'response': 'Wrong name or pw'}), 401
-
-
-@app.route('/logout/')
-def logout():
-	session.pop('user_id', None)
-	return redirect(url_for('home'))
+    return render_template('login.html')
 
 
 @app.route('/test/')
 def test():
-	return render_template('test.html')
+    return render_template('test.html')
 
 
 @app.route('/steven/')
 def steven():
-	return render_template('steven.html')
+    return render_template('steven.html')
 
 
 # API
-@app.route('/api/demo/getAudio', methods=['POST'])
-def api_demo_getAudio():
-	filePath = 'static/test.wav'
-	
-	# return jsonify({'response': 'hi'}), 200
-	return jsonify(send_file(filePath, 'audio/wav', True, 'tacotron-ch-92000-0.wav')), 200
+# AUTH
+@app.route('/api/auth/login', methods=['POST'])
+def api_auth_login():
+    request_data = request.get_json()
 
-# API using Response
-#@app.route('/api/demo/wav', methods=['GET', 'POST'])
-#def streamwav():
-#	def generate():
-#        	with open("/static/test.wav", "rb") as fwav:
-#	           	data = fwav.read(1024)
-#        	    	while data:
-#                		yield data
-#                		data = fwav.read(1024)
-#    return Response(generate(), mimetype="audio/x-wav")
+    try:
+        if 'id' not in request_data or 'password' not in request_data:
+            return jsonify({'response': 'request data error'}), 400
+
+        if request_data['id'] in users and request_data['password'] == users[request_data['id']]['password']:
+            session['user_id'] = request_data['id']
+            return jsonify({'response': 'login success'}), 200
+
+        if request_data['id'] in users and request_data['password'] != users[request_data['id']]['password']:
+            session['user_id'] = request_data['id']
+            return jsonify({'response': 'wrong password'}), 401
+
+        if request_data['id'] not in users:
+            return jsonify({'response': 'user not exists'}), 401
+
+    except Exception as err:
+        print('[ERROR - api/auth/login]', err)
+        return jsonify({'response': 'undefined error'}), 500
+
+
+@app.route('/api/auth/logout', methods=['GET'])
+def api_auth_logout():
+    request_data = request.get_json()
+
+    try:
+        if request_data:
+            return jsonify({'response': 'request data error'}), 400
+
+        session.pop('user_id', None)
+        return jsonify({'response': 'logout success'}), 200
+
+    except Exception as err:
+        print('[ERROR - api/auth/logout]', err)
+        return jsonify({'response': 'undefined error'}), 500
+
 
 # API Steven test 2
 @app.route('/api/test2', methods=['POST'])
 def view_method():
-     path_to_file = "/static/test.wav"
+    path_to_file = "/static/test.wav"
 
-     return send_file(
-         path_to_file, 
-         mimetype="audio/wav", 
-         as_attachment=True, 
-         attachment_filename="test.wav")
+    return send_file(
+        path_to_file,
+        mimetype="audio/wav",
+        as_attachment=True,
+        attachment_filename="test.wav")
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=8080)
