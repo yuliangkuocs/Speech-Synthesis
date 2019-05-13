f03bb9d5d838b7e4e9263654cb69eb2a2cadbd23
yuliangkuocs <yuliangkuocs@gmail.com>
2019-05-13 21:40:28 +0800

    create new branch Web

--- TransformWave.py
+++ TransformWave.py
@@ -5,13 +5,13 @@ from scipy.io.wavfile import write
 
 
 DATA_PATH = './WaveData/'
-SAMPLE_RATE = 16000
+SAMPLE_RATE = 48000
 
 waveDatas = glob(DATA_PATH + '*.npy')
 
 
 for data in waveDatas:
-    outputPath = os.path.dirname(__file__) + '/Result/'
+    outputPath = os.path.dirname(__file__) + 'Result/'
     waveNpData = np.load(data)
     write(outputPath + 'result-{0}.wav'.format(data.replace(DATA_PATH, '').replace('.npy', '')), SAMPLE_RATE, waveNpData)
 
