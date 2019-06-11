04025b771240f627f3c0cea9d01ce918b85489f3
yuliangkuocs <yuliangkuocs@gmail.com>
2019-06-12 00:55:26 +0800

    test page finish tts english ljspeech api

--- web/path.py
+++ web/path.py
@@ -1,5 +1,6 @@
 PATH = {
     'mandarin_BZNSYP': '/home/steven/Desktop/TTS-Mandarin',
+    'english_LJSpeech': '/home/steven/Desktop/TTS-English',
     'M_AILABS_mary_ann': '/home/steven/Desktop/TTS-English',
     'web_static': '/home/steven/Desktop/Speech-Synthesis/web/static'
 }
